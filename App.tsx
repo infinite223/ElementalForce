5a92e7a4cf9f37a48978e702bf2104c40e14b62a
infinite223 <dawidszmigiel9@gmail.com>
2023-11-14 23:39:05 +0100

    init project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { colors } from './src/Utils/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.appName}>
+        Elemental
+        <Text style={{color: colors.provideColor}}>
+          Force
+        </Text>
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Wejdź do gry
+        </Text>
+      </TouchableOpacity> 
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20
   },
+  appName: {
+    fontSize: 30,
+    fontWeight:'900',
+    fontStyle: 'italic'
+  },
+  button: {
+    borderRadius: 50,
+    padding: 10,
+    paddingHorizontal: 20,
+    backgroundColor: colors.provideColor
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 17
+  }
 });
---
+++ src/Utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+    provideColor: '#fc3523'
+} 
